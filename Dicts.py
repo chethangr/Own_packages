fe359e5448ced77049f8652a6cba2e8d98900dce
Chethan GR <81103456+chethangr@users.noreply.github.com>
2022-01-29 23:25:54 +0530

    Add files via upload

---
+++ Dicts.py
@@ -0,0 +1,25 @@
+import logging
+
+
+class Dicts:
+    try:
+
+        logging.basicConfig (filename="My_Packages.log", level=logging.DEBUG, format='%(asctime)s %(levelname)s:'
+                                                                                     '%(message)s')
+
+        def __init__(self, d1):
+            self.d1 = d1
+
+        def my_keys(self):
+            for i in self.d1:
+                print (i)
+
+        def my_values(self):
+            for i in self.d1:
+                print (self.d1[i])
+        logging.info (f"Dicts function has  Executed successfully!")
+
+    except Exception as e:
+        logging.error ("Didn't execute successfully")
+        logging.debug ("Please check the entered value")
+        logging.exception ("Exception has occurred: " + str (e))
--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from Lists import *
-
 from Dicts import *
 from Tuples import *
 
 l = Lists ([1, 2, 3, 45, 6])
 
 # print(l.l_insert())
-
 # print(l.l_count())
 print(l.l_reverse())
-
 # print(l.l_copy())
 
-# d = Dicts({"a": 12,
-#                "b": 13,
-#                "c": 14,
-#                "d": 15})
+d = Dicts({"a": 12,
+               "b": 13,
+               "c": 14,
+               "d": 15})
 #
 #
-# d.my_keys()
-
+d.my_keys()
+d.my_values()
 t = Tuples((1,2,5,3,5,2,5), v=0)
 
-# print(t.my_count(7))
-# print(t.my_index("2"))
+print(t.my_count(5))
 t.my_index(2)
 
 
